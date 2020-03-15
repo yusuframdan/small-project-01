01be1a3cbb80ebdc98114bee37d26de577e19c5d
yusuf ramdan <yusufrmp@gmail.com>
2020-03-15 06:01:27 -0700

    presentase SP 01

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+$('#myTab a').on('click', function(e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
+$('#myTab a[href="#profile"]').tab('show') // Select tab by name
+$('#myTab li:first-child a').tab('show') // Select first tab
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
